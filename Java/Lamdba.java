995e4c7c59899d38d4a71ba75d334d7f43e4350a
RastW <ackermanx@163.com>
2021-05-13 17:26:30 +0800

    Java8 -Lambda

--- Java/Lamdba.java
+++ Java/Lamdba.java
@@ -8,6 +8,7 @@
 //
 // 2. 函数饮用
 // 饮用一个已经存在的方法，以此来代替要实现的函数的饮用
+// 阅读此文件请根据main方法进行阅读，从上往下依次是从基础到深入的代码实现
 public class Program {
 
 	public static void main(String[] args) {
@@ -93,6 +94,58 @@ public class Program {
 		SingleReturnMutipleParameter lambda2 = (a, b) -> a * b;
 	}
 
+	// 引用函数
+	private static void LambdaFunctionReference() {
+		// 使用lambda基础进行方法调用
+		SingleReturnMutipleParameter lambda1 = (x, y) -> calculate(x, y);
+		System.out.println(lambda1.use(10, 20));
+
+		// 使用静态函数引用
+		// 类名::静态方法名
+		// 1. 函数引用引用的方法参数与返回值必须相同
+		// 2. 方法名不加小括号
+		SingleReturnMutipleParameter lambda2 = Program::calculate;
+		System.out.println(lambda2.use(10, 20));
+
+		// 使用非静态方法
+		// 对象::非静态方法名
+		Program program = new Program();
+		SingleReturnMutipleParameter lambda3 = program::calculate2;
+		SingleReturnMutipleParameter lambda4 = new Program()::calculate2;
+		System.out.println(lambda3.use(10, 20) + lambda4.use(20, 10));
+
+		// 使用引用函数 引用构造方法
+		// 类::new
+		GetPersonWithNoneParameter getPerson1 = Person::new;
+		GetPersonWithMutipleParameter getPerson2 = Person::new;
+		Person person = getPerson1.get();
+
+		// 当使用一个对象作为参数，调用对象中的方法可以完成语句时，可以简写
+		GetPersonName getPersonName = person -> person.getName();
+		// 可以简写为:
+		GetPersonName getPersonName = Person::getName;
+	}
+
+
+
+	private static void calculate(int x, int y) {
+		if (x > y) {
+			return x - y;
+		} else if (x > x) {
+			return y - x;
+		}
+		return x + y;
+	}
+ 
+	private int calculate2(int x, int y) {
+		if (x > y) {
+			return x - y;
+		} else if (x > x) {
+			return y - x;
+		}
+		return x + y;
+	}
+
 }
 
 
@@ -147,7 +200,45 @@ public interface SingleReturnSingleParameter {
 }
 
 // 单返回多参数
-@FunctionINterface
+@FunctionInterface
 public interface SingleReturnMutipleParameter {
 	int use(int a, int b);
+}
+
+// ---------------------------------- Persion类, 用于展示lambda构造方法引用 --------------------------------------
+public class Person {
+	private String name;
+	private int age;
+
+	public Person() {
+		Syste.out.println("Person 无参构造方法");
+	}
+
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+		System.out.println("Person 有参构造方法");
+	}
+
+	public String getName() {
+		return this.name;
+	}
+}
+
+
+// 无参构造
+@FunctionInterface
+public interface GetPersonWithNoneParameter {
+	Person get();
+}
+
+// 有参构造
+@FunctionInterface
+public interface GetPersonWithMutipleParameter {
+	Person get(String name, int age);
+}
+
+@FunctionInterface
+public interface GetPersonName {
+	String get();
 }
