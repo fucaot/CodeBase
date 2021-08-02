5830198bf31999a0cddcf4ab72e765067a3f1b65
RastW <ackermanx@163.com>
2021-08-02 17:35:40 +0800

    rresponseWrite_test.go add Test_Write func

--- Golang/src/net/http/rresponseWrite_test.go
+++ Golang/src/net/http/rresponseWrite_test.go
@@ -6,13 +6,22 @@ import (
 )
 
 func Test_Write(t *testing.T) {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/write", func(w http.ResponseWriter, r *http.Request) {
 		// 使用Write写入byte[]切片
 		// 如果未设置Content-Type, 则前512字节用来检测Content-Type
+		str := `<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+		<root>
+			<name >asaa</name >
+			<das>wjkkj</das>
+		</root>`
+		w.Write([]byte(str))
+
+		// 执行 curl -i localhost:8080/write 试试
 	})
+	http.ListenAndServe("localhost:8080", nil)
 }
 
-func NotFound(t *testing.T) {
+func TestNotFound(t *testing.T) {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 	})
 }
